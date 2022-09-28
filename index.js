cd1251f51d76485307eb31b069e181b042f8d840
AbdelHamid Reda <101983081+oxygen000@users.noreply.github.com>
2022-09-28 11:07:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const labels = document.querySelectorAll(".form-control label");
+labels.forEach((labels)=>{
+    labels.innerHTML= labels.innerText
+    .split("")
+    .map(
+        (letter, idx)=>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+
+});
